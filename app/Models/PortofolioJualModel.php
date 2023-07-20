9b65b9c59200c3d3094d33c967609173ed53b5f1
Dimas Bagus <dimasbagusk03@gmail.com>
2023-07-21 03:52:07 +0800

    fix commits issues

--- app/Models/PortofolioJualModel.php
+++ app/Models/PortofolioJualModel.php
@@ -10,7 +10,6 @@ class PortofolioJualModel extends Model
 
     protected $table = "tb_portofolio_jual";
     protected $fillable = [
-<<<<<<< HEAD
         'id_saham',
         'user_id',
         'jenis_saham',
@@ -19,9 +18,6 @@ class PortofolioJualModel extends Model
         'harga_jual',
         'id_sekuritas',
         'close_persen'
-=======
-        'id_saham', 'user_id', 'jenis_saham', 'volume', 'tanggal_jual', 'harga_jual', 'fee_jual_persen', 'close_persen','id_sekuritas'
->>>>>>> b3833b0 (edit sumtin)
     ];
     public $timestamps = false;
     protected $primaryKey = 'id_portofolio_jual';
