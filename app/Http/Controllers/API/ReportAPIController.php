e430777339232781a643e5de6d79ff935979fcee
adam-breda <breda.adam@gmail.com>
2023-08-09 01:41:17 +0800

    Update ReportAPIController.php

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -81,6 +81,7 @@ class ReportAPIController extends Controller
 
             if (!$jualReport) {
                 $data[$i]['total_volume_jual'] = 0;
+                $data[$i]['total_volume_beli'] = 0;
                 $data[$i]['avg_harga_jual'] = 0;
                 $data[$i]['total_volume'] = $data[$i]['total_volume_beli'];
                 $data[$i]['keuntungan'] = 100 * (($data[$i]['total_volume']*$data[$i]['avg_harga_beli'])-($data[$i]['total_volume']*$hargaclose ));
@@ -91,6 +92,7 @@ class ReportAPIController extends Controller
 
             } else {
                 $data[$i]['total_volume_jual'] = $jualReport[0]['total_volume_jual'];
+                $data[$i]['total_volume_beli'] = $jualReport[0]['total_volume_beli'];
                 $data[$i]['avg_harga_jual'] = round($jualReport[0]['avg_harga_jual']);
                 $data[$i]['total_volume'] = $data[$i]['total_volume_beli']-$jualReport[0]['total_volume_jual'];
                 //$data[$i]['keuntungan'] = 0;
