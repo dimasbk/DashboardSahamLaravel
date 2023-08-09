4125e55c2635b0538bfd0a931fe1e9c21e581cc0
adam-breda <breda.adam@gmail.com>
2023-08-10 01:32:30 +0800

    Update ReportAPIController.php

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -180,7 +180,7 @@ class ReportAPIController extends Controller
                 $data[$i]['sisa_aset'] = ($data[$i]['total_lot']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_lot']*$data[$i]['avg_harga_jual']);
                // $data[$i]['sisa_aset'] = (100*($data[$i]['total_volume']));
                 $data[$i]['harga_close'] = $hargaclose;
-                $data[$i]['total_banget'] = $data[$i]['total_beli_banget'] + $jualReport[0]['total_jual_banget'];
+                $data[$i]['total_banget'] = $data[$i]['total_beli_banget'] - $jualReport[0]['total_jual_banget'];
 
                 $data[$i]['total_volume_jual'] = (string)$data[$i]['total_volume_jual'];
                 $data[$i]['avg_harga_jual'] = (string)$data[$i]['avg_harga_jual'];
@@ -202,7 +202,7 @@ class ReportAPIController extends Controller
                 $data[$i]['sisa_aset'] = ($data[$i]['total_lot']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_lot']*$data[$i]['avg_harga_jual']);
                 $data[$i]['harga_close'] = $hargaclose;
                 $data[$i]['year'] = $year;
-                $data[$i]['total_banget'] = $data[$i]['total_beli_banget'] + $jualReport[0]['total_jual_banget'];
+                $data[$i]['total_banget'] = $data[$i]['total_beli_banget'];
                 // $data[$i]['keuntungan'] = (string)$data[$i]['keuntungan'];
 
 
