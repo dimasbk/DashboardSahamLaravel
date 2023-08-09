7990d0ed31d50938cfa79ee466ab9e10b419aec0
adam-breda <breda.adam@gmail.com>
2023-08-09 18:36:28 +0800

    Update ReportAPIController.php

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -175,8 +175,8 @@ class ReportAPIController extends Controller
                 $data[$i]['avg_harga_jual'] = ($jualReport[0]['avg_harga_jual']);
                 $data[$i]['total_volume'] = $data[$i]['total_volume_beli']-$jualReport[0]['total_volume_jual'] ;
                 //$data[$i]['keuntungan'] = 0;
-                $data[$i]['keuntungan'] = (100*($data[$i]['total_volume'] *$data[$i]['avg_harga_beli'] ) - ($data[$i]['total_volume'] *$hargaclose)) ;
-                $data[$i]['sisa_aset'] = (100*($data[$i]['total_volume']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_volume']*$data[$i]['avg_harga_jual']));
+                $data[$i]['keuntungan'] = (100*(($data[$i]['total_volume'] *$data[$i]['avg_harga_beli'] ) - ($data[$i]['total_volume'] *$hargaclose))) ;
+                $data[$i]['sisa_aset'] = (100*(($data[$i]['total_volume']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_volume']*$data[$i]['avg_harga_jual'])));
                // $data[$i]['sisa_aset'] = (100*($data[$i]['total_volume']));
                 $data[$i]['harga_close'] = $hargaclose;
 
@@ -194,7 +194,7 @@ class ReportAPIController extends Controller
                 $data[$i]['total_volume'] = $data[$i]['total_volume_beli'];
                 $data[$i]['keuntungan'] = 100 * (($data[$i]['total_volume']*$data[$i]['avg_harga_beli'])-($data[$i]['total_volume']*$hargaclose ));
                // $data[$i]['sisa_aset'] = 100*$data[$i]['total_volume']*$data[$i]['avg_harga_beli'];
-                $data[$i]['sisa_aset'] = (100*($data[$i]['total_volume']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_volume']*$data[$i]['avg_harga_jual']));
+                $data[$i]['sisa_aset'] = (100*(($data[$i]['total_volume']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_volume']*$data[$i]['avg_harga_jual'])));
                 $data[$i]['harga_close'] = $hargaclose;
                 $data[$i]['year'] = $year;
                 // $data[$i]['keuntungan'] = (string)$data[$i]['keuntungan'];
