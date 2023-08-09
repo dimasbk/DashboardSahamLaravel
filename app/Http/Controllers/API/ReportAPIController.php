187ccdfee707f9e9427f278d2165708e83b126c3
adam-breda <breda.adam@gmail.com>
2023-08-10 02:04:46 +0800

    Update ReportAPIController.php

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -79,21 +79,7 @@ class ReportAPIController extends Controller
                // return $untung;
 
 
-            if (!$jualReport) {
-                $data[$i]['total_volume_jual'] = 0;
-                $data[$i]['avg_harga_jual'] = 0;
-                $data[$i]['total_volume'] = $data[$i]['total_volume_beli'];
-                $data[$i]['total_lot'] = ($data[$i]['total_volume_beli']) * 100;
-                $data[$i]['keuntungan'] =  ($data[$i]['total_lot']*$data[$i]['avg_harga_beli'])-($data[$i]['total_lot']*$hargaclose);
-               // $data[$i]['sisa_aset'] = 100*$data[$i]['total_lot']*$data[$i]['avg_harga_beli'];
-                $data[$i]['sisa_aset'] = ($data[$i]['total_lot']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_lot']*$data[$i]['avg_harga_jual']);
-                $data[$i]['harga_close'] = $hargaclose;
-                $data[$i]['year'] = $year;
-                $data[$i]['total_banget'] = $data[$i]['total_beli_banget'];
-
-                // $data[$i]['keuntungan'] = (string)$data[$i]['keuntungan'];
-
-            } else {
+               if ($jualReport != null){
                 $data[$i]['total_volume_jual'] = $jualReport[0]['total_volume_jual'];
                 $data[$i]['avg_harga_jual'] = ($jualReport[0]['avg_harga_jual']);
                 $data[$i]['total_volume'] = $data[$i]['total_volume_beli']-$jualReport[0]['total_volume_jual'] ;
@@ -112,7 +98,24 @@ class ReportAPIController extends Controller
                // $data[$i]['total_banget'] = (string)$data[$i]['total_banget'];
                 $data[$i]['year'] = $year;
                 $data[$i]['ayam'] = $year;
+
                // $data[$i]['sisa_aset'] = (string)$data[$i]['sisa_aset'];
+            }
+            if (!$jualReport) {
+                $data[$i]['total_volume_jual'] = 0;
+                $data[$i]['avg_harga_jual'] = 0;
+                $data[$i]['total_volume'] = $data[$i]['total_volume_beli'];
+                $data[$i]['total_lot'] = ($data[$i]['total_volume_beli']) * 100;
+                $data[$i]['keuntungan'] =  ($data[$i]['total_lot']*$data[$i]['avg_harga_beli'])-($data[$i]['total_lot']*$hargaclose);
+               // $data[$i]['sisa_aset'] = 100*$data[$i]['total_lot']*$data[$i]['avg_harga_beli'];
+                $data[$i]['sisa_aset'] = ($data[$i]['total_lot']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_lot']*$data[$i]['avg_harga_jual']);
+                $data[$i]['harga_close'] = $hargaclose;
+                $data[$i]['year'] = $year;
+                $data[$i]['total_banget'] = $data[$i]['total_beli_banget'];
+                // $data[$i]['keuntungan'] = (string)$data[$i]['keuntungan'];
+
+
+
             }
         }
 
