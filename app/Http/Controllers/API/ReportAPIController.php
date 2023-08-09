977e859607884511636241fc29c44a7a13642b49
adam-breda <breda.adam@gmail.com>
2023-08-09 18:45:41 +0800

    Update ReportAPIController.php

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -174,9 +174,10 @@ class ReportAPIController extends Controller
                 $data[$i]['total_volume_jual'] = $jualReport[0]['total_volume_jual'];
                 $data[$i]['avg_harga_jual'] = ($jualReport[0]['avg_harga_jual']);
                 $data[$i]['total_volume'] = $data[$i]['total_volume_beli']-$jualReport[0]['total_volume_jual'] ;
+                $data[$i]['total_lot'] = ($data[$i]['total_volume_beli']-$jualReport[0]['total_volume_jual']) * 100 ;
                 //$data[$i]['keuntungan'] = 0;
-                $data[$i]['keuntungan'] = (100*(($data[$i]['total_volume'] *$data[$i]['avg_harga_beli'] ) - ($data[$i]['total_volume'] *$hargaclose))) ;
-                $data[$i]['sisa_aset'] = (100*(($data[$i]['total_volume']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_volume']*$data[$i]['avg_harga_jual'])));
+                $data[$i]['keuntungan'] = ($data[$i]['total_lot'] *$data[$i]['avg_harga_beli'] ) - ($data[$i]['total_lot'] *$hargaclose) ;
+                $data[$i]['sisa_aset'] = ($data[$i]['total_lot']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_lot']*$data[$i]['avg_harga_jual']);
                // $data[$i]['sisa_aset'] = (100*($data[$i]['total_volume']));
                 $data[$i]['harga_close'] = $hargaclose;
 
@@ -192,9 +193,10 @@ class ReportAPIController extends Controller
                 $data[$i]['total_volume_jual'] = 0;
                 $data[$i]['avg_harga_jual'] = 0;
                 $data[$i]['total_volume'] = $data[$i]['total_volume_beli'];
-                $data[$i]['keuntungan'] = 100 * (($data[$i]['total_volume']*$data[$i]['avg_harga_beli'])-($data[$i]['total_volume']*$hargaclose ));
-               // $data[$i]['sisa_aset'] = 100*$data[$i]['total_volume']*$data[$i]['avg_harga_beli'];
-                $data[$i]['sisa_aset'] = (100*(($data[$i]['total_volume']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_volume']*$data[$i]['avg_harga_jual'])));
+                $data[$i]['total_lot'] = ($data[$i]['total_volume_beli']) * 100;
+                $data[$i]['keuntungan'] =  ($data[$i]['total_lot']*$data[$i]['avg_harga_beli'])-($data[$i]['total_lot']*$hargaclose);
+               // $data[$i]['sisa_aset'] = 100*$data[$i]['total_lot']*$data[$i]['avg_harga_beli'];
+                $data[$i]['sisa_aset'] = ($data[$i]['total_lot']*$data[$i]['avg_harga_beli']) - ($data[$i]['total_lot']*$data[$i]['avg_harga_jual']);
                 $data[$i]['harga_close'] = $hargaclose;
                 $data[$i]['year'] = $year;
                 // $data[$i]['keuntungan'] = (string)$data[$i]['keuntungan'];
