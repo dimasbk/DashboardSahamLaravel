772af480686d08f105be5f55594cb00d044f9676
adam-breda <breda.adam@gmail.com>
2023-08-10 19:03:20 +0800

    Update ReportAPIController.php

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -432,6 +432,8 @@ class ReportAPIController extends Controller
 
         $total_semua =  ($total_semua_beli*$beli_total - $total_semua_jual*$jual_total) + (($avgJual * $jual_total)*100);
 
+        $persentase_profit = ($realisasi/$avgBeli*100)*100;
+
         if ($function === 1) {
             return compact(['keuntungan', 'realisasi', 'total_semua']);
         }
