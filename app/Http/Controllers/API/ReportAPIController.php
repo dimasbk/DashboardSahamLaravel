ab425f5c112baa7dacff9bf438c5227edcc88d90
adam-breda <breda.adam@gmail.com>
2023-08-10 13:53:00 +0800

    Update ReportAPIController.php

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -818,19 +818,19 @@ class ReportAPIController extends Controller
     public function ThisYear(Request $request)
     {
         $id_user = Auth::id();
-        // $tahun = PortofolioBeliModel::selectRaw('EXTRACT(YEAR FROM tanggal_beli) as tahun')
-        //     ->where('tb_portofolio_beli.user_id', Auth::id())
-        //     ->groupBy(DB::raw('EXTRACT(YEAR FROM tanggal_beli)'))
-        //     ->get()->toArray();
+        $tahun = PortofolioBeliModel::selectRaw('EXTRACT(YEAR FROM tanggal_beli) as tahun')
+            ->where('tb_portofolio_beli.user_id', Auth::id())
+            ->groupBy(DB::raw('EXTRACT(YEAR FROM tanggal_beli)'))
+            ->get()->toArray();
 
-        // $currentYear = date('Y'); // Get the current year
+        $currentYear = date('Y'); // Get the current year
 
-        // $filteredArray = array_filter($tahun, function ($item) use ($currentYear) {
-        //     return $item['tahun'] != $currentYear;
-        // });
+        $filteredArray = array_filter($tahun, function ($item) use ($currentYear) {
+            return $item['tahun'] != $currentYear;
+        });
 
-        // $filteredArray = array_values($filteredArray);
-        // $tahun = $filteredArray;
+        $filteredArray = array_values($filteredArray);
+        $tahun = $filteredArray;
        // return $tahun;
        // $tahun = ["Tahun: 2023"];
         $tahun = ['tahun' => '2023'];
@@ -907,7 +907,7 @@ class ReportAPIController extends Controller
 
         return response()->json([
             'status' => 'success',
-            'data' => $tahun
+            'data' => $data
         ], 200);
     }
 
